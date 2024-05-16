0734f96c34b4748a081121668a46801ed29957a6
Hiroki Noda <kubo39@gmail.com>
2024-05-17 02:15:07 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# SIEVE cache in D
+
+An implementation of the [SIEVE][1] Cache algorithm for D.
+This implementation is fully inspired by [Rust's sieve-cache implementation][2].
+
+## Run tests
+
+```console
+dub test
+```
+
+## Run lint
+
+```console
+dub lint
+```
+
+## Build the docment
+
+```console
+dub run -b ddox
+```
+
+[1]: https://cachemon.github.io/SIEVE-website/
+[2]: https://github.com/jedisct1/rust-sieve-cache
---
+++ source/sievecache/package.d
@@ -0,0 +1,239 @@
+/**
+ * This is an implementation of the [SIEVE](https://cachemon.github.io/SIEVE-website)
+ * cache replacement algorithm for D.
+ *
+ * Authors: Hiroki Noda
+ * Copyright: Copyright © 2024 Hiroki Noda
+ * License: MIT
+ */
+module sievecache;
+
+@safe:
+
+private:
+
+import std.exception : enforce;
+import std.traits : isEqualityComparable, isSomeFunction, isTypeTuple;
+
+enum bool isHashable(T) =
+    !(is(isSomeFunction!T)
+        || is(T == void)
+        || is(isTypeTuple!T));
+
+public:
+
+/**
+ * A cache based on the SIEVE eviction algorithm.
+ */
+struct SieveCache(K, V)
+    if (isEqualityComparable!K && isHashable!K)
+{
+    /**
+     * Create a new cache with given capacity.
+     */
+    this(size_t capacity) pure
+    {
+        enforce(capacity > 0, "capacity must be greater than zero.");
+        capacity_ = capacity;
+        length_ = 0;
+    }
+
+    /**
+     * Returns the capacity of the cache.
+     */
+    size_t capacity() const @nogc nothrow pure
+    {
+        return capacity_;
+    }
+
+    /**
+     * Returns the length of the cache.
+     */
+    size_t length() const @nogc nothrow pure
+    {
+        return length_;
+    }
+
+    /**
+     * Returns `true` when no value are currently cached.
+     */
+    bool empty() const @nogc nothrow pure
+    {
+        return length_ == 0;
+    }
+
+    /**
+     * Returns `true` if there is a value in the cache mapped to
+     * by `key`.
+     */
+    bool contains(K key) const @nogc nothrow pure
+    {
+        return (key in aa_) !is null;
+    }
+
+    /**
+     * Yields a pointer to the value in the cache mapped to
+     * by `key`.
+     * If no value exists for `key`, returns `null`.
+     */
+    scope V* get(K key) @nogc nothrow pure
+    {
+        Node!(K, V)** nodePtr = key in aa_;
+        if (nodePtr is null)
+        {
+            return null;
+        }
+        (*nodePtr).visited = true;
+        return &(*nodePtr).value;
+    }
+
+    /**
+     * Map `key` to `value` in the cache, possibly evicting old entries.
+     * Returns `true` when this is a new entry, and `false` if an existing
+     * entry was updated.
+     */
+    bool insert(K key, V value) nothrow pure
+    {
+        Node!(K, V)** nodePtr = key in aa_;
+        if (nodePtr !is null)
+        {
+            (**nodePtr).value = value;
+            return false;
+        }
+        if (length_ >= capacity_)
+        {
+            evict();
+        }
+        Node!(K, V)* node = new Node!(K, V)(key, value);
+        addNode(node);
+        aa_[key] = node;
+        assert(length_ < capacity_);
+        length_++;
+        return true;
+    }
+
+    /**
+     * Removes the cache entry mapped to by `key`.
+     * Returns the value remove from the cache. If `key` did not map to
+     * any value, then this returns `null`.
+     */
+    bool remove(K key) @nogc nothrow pure
+    {
+        Node!(K, V)** nodePtr = key in aa_;
+        if (nodePtr is null)
+        {
+            return false;
+        }
+        Node!(K, V)* node = *nodePtr;
+        removeNode(node);
+        assert(length_ > 0);
+        length_--;
+        return aa_.remove(key);
+    }
+
+private:
+    void addNode(Node!(K, V)* node) @nogc nothrow pure
+    {
+        node.next = head_;
+        node.prev = null;
+        if (head_ !is null)
+        {
+            head_.prev = node;
+        }
+        head_ = node;
+        if (tail_ is null)
+        {
+            tail_ = head_;
+        }
+    }
+
+    void removeNode(Node!(K, V)* node) @nogc nothrow pure
+    {
+        if (node.prev !is null)
+        {
+            node.prev.next = node.next;
+        }
+        else
+        {
+            head_ = node.next;
+        }
+
+        if (node.next !is null)
+        {
+            node.next.prev = node.prev;
+        }
+        else
+        {
+            tail_ = node.prev;
+        }
+    }
+
+    void evict() @nogc nothrow pure
+    {
+        Node!(K, V)* node = null;
+        if (hand_ !is null)
+        {
+            node = hand_;
+        }
+        else if (tail_ !is null)
+        {
+            node = tail_;
+        }
+        while (node !is null)
+        {
+            if (!node.visited)
+            {
+                break;
+            }
+            node.visited = false;
+            if (node.prev !is null)
+            {
+                node = node.prev;
+            }
+            else
+            {
+                node = tail_;
+            }
+        }
+
+        if (node !is null)
+        {
+            hand_ = node.prev;
+            aa_.remove(node.key);
+            removeNode(node);
+            assert(length_ > 0);
+            length_--;
+        }
+    }
+
+    Node!(K, V)*[K] aa_;
+    Node!(K, V)* head_;
+    Node!(K, V)* tail_;
+    Node!(K, V)* hand_;
+    size_t capacity_;
+    size_t length_;
+
+    struct Node(K, V)
+    {
+        K key;
+        V value;
+        Node!(K, V)* prev;
+        Node!(K, V)* next;
+        bool visited;
+    }
+}
+
+@("smoke test")
+unittest
+{
+    auto cache = SieveCache!(string, string)(3);
+    cache.insert("foo", "foocontent");
+    cache.insert("bar", "barcontent");
+    assert(cache.remove("bar"));
+    cache.insert("bar2", "bar2content");
+    cache.insert("bar3", "bar3content");
+    assert(*cache.get("foo") == "foocontent");
+    assert(cache.get("bar") is null);
+    assert(*cache.get("bar2") == "bar2content");
+    assert(*cache.get("bar3") == "bar3content");
+}
