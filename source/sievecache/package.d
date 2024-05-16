7ef18c1a9a0a82b26751a310b43ddcae2f8be104
Hiroki Noda <kubo39@gmail.com>
2024-05-17 03:07:56 +0900

    dub run dfmt -- -i source/

--- source/sievecache/package.d
+++ source/sievecache/package.d
@@ -16,18 +16,14 @@ import std.exception : enforce;
 import std.traits : isEqualityComparable, isSomeFunction, isTypeTuple;
 
 // https://github.com/dlang/dmd/blob/5b5fba8b6af4a3b66f1cf0c2157a69305094ff27/compiler/src/dmd/typesem.d#L1598-L1605
-enum bool isKeyableType(T) =
-    !(is(isSomeFunction!T)
-        || is(T == void)
-        || is(isTypeTuple!T));
+enum bool isKeyableType(T) = !(is(isSomeFunction!T) || is(T == void) || is(isTypeTuple!T));
 
 public:
 
 /**
  * A cache based on the SIEVE eviction algorithm.
  */
-struct SieveCache(K, V)
-    if (isEqualityComparable!K && isKeyableType!K)
+struct SieveCache(K, V) if (isEqualityComparable!K && isKeyableType!K)
 {
     /**
      * Create a new cache with given capacity.
